52a84865e45c9a2b455d7a653761cf3a99d5e8bc
Dane Rosa <dane.rosa@panther.io>
2022-12-02 22:37:55 -0500

    my code

--- main.py
+++ main.py
@@ -3,6 +3,44 @@ Codebreaker
 """
 
 
+def dr_test(x, y, box, code, old_guesses, count):
+    for e in range(x):
+        guess_code = []
+        hint = []
+        print('Pick from R, B, Y, G, O, or P')
+        print('Type quit to end early')
+        for i in range(y):
+            guess = (input(f'Guess the {i + 1} position: ')).upper()
+            if guess == 'QUIT':
+                return print("You ended early, better luck next time")
+            while guess not in set(box):
+                print('Invalid input. Guess must be from R, B, Y, G, O or P')
+                guess = (input(f'Guess the {i + 1} position: ')).upper()
+            guess_code.append(guess)
+        guess_copy = guess_code.copy()
+        code_copy = code.copy()
+        for i, e in enumerate(guess_copy):
+            if guess_copy[i] == code_copy[i]:
+                hint.append('Black')
+            elif guess_copy[i] in code_copy:
+                hint.append('White')
+            else:
+                hint.append('-')
+        old_guesses.update({
+            str(guess_code): hint
+        })
+        print('Hints: Black means a guess is correct AND in the correct position')
+        print('Hints: White means a guess is correct BUT in the wrong position')
+        for key, value in old_guesses.items():
+            print(key, value)
+        count += 1
+        if guess_code == code:
+            return print('Congrats! You correctly guessed',
+                         f' {code} in {count} attempts!')
+    return print(f'Unlucky! You failed to guess {code}',
+                 f'in {count} attempts')
+
+
 def codebreaker():
     import random
     x = 0
@@ -29,51 +67,13 @@ def codebreaker():
             x = int(input('How many guesses would you like to attempt?: '))
         except ValueError:
             print(f'Invalid input, use an integer greater than 0')
-    while y < 1 or y > 72:
+    while y not in range(1, 72):
         try:
             y = int(input('How long do you want the code? (From 1 to 72): '))
         except ValueError:
             print(f'Invalid input, use an integer from 1 to 72')
-    for i in range(y):
-        code.append(box.pop(0))
-    for e in range(x):
-        guess_code = []
-        hint = []
-        print('Pick from R, B, Y, G, O, or P')
-        print('Type quit to end early')
-        for i in range(y):
-            guess = (input(f'Guess the {i + 1} position: ')).upper()
-            if guess == 'QUIT':
-                return print("You ended early, better luck next time")
-            while guess != 'R' and guess != 'B' and guess != 'Y' and guess != 'G' and guess != 'O' and guess != 'P':
-                print('Invalid input. Guess must be from R, B, Y, G, O or P')
-                guess = (input(f'Guess the {i + 1} position: ')).upper()
-            guess_code.append(guess)
-        guess_copy = guess_code.copy()
-        code_copy = code.copy()
-        for i, e in enumerate(guess_copy):
-            if guess_copy[i] == code_copy[i]:
-                hint.append('Black')
-                code_copy[i] = 'X'
-                guess_copy[i] = 'Z'
-        for i, e in enumerate(guess_copy):
-            if guess_copy[i] != code_copy[i] and e in code_copy:
-                hint.append('White')
-                code_copy[i] = 'X'
-                guess_copy[i] = 'Z'
-        hint.sort()
-        old_guesses[str(guess_code)] = hint
-        print('Hints: Black means a guess is correct AND in the correct position')
-        print('Hints: White means a guess is correct BUT in the wrong position')
-        for key, value in old_guesses.items():
-            print(key, value)
-        count = count + 1
-        if guess_code == code:
-            return print('Congrats! You correctly guessed',
-                         f' {code} in {count} attempts!')
-    return print(f'Unlucky! You failed to guess {code}',
-                 f'in {count} attempts')
-
+    code = box[:y]
+    dr_test(x,y,box,code,old_guesses,count)
 
 if __name__ == '__main__':
     codebreaker()
